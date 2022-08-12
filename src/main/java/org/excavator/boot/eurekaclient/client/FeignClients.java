a21c406321b49cc9151580d3d0c632c4b2a1c5b5
CMonkey <42.codemonkey@gmail.com>
2022-08-12 18:27:28 +0800

    make ReactiveFeignClient Annotation

--- src/main/java/org/excavator/boot/eurekaclient/client/FeignClients.java
+++ src/main/java/org/excavator/boot/eurekaclient/client/FeignClients.java
@@ -1,12 +1,15 @@
 package org.excavator.boot.eurekaclient.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import reactor.core.publisher.Mono;
+import reactivefeign.spring.config.ReactiveFeignClient;
 public class FeignClients {
-    @FeignClient("eureka-client-service")
+    @ReactiveFeignClient(name = "eureka-client-service")
     public interface GreetingClient{
-        @RequestMapping("/greeting/greeting")
-        String greeting();
+        @GetMapping(value = "/greeting/greeting",produces = MediaType.APPLICATION_JSON_VALUE)
+        Mono<String> greeting();
     }
 }
