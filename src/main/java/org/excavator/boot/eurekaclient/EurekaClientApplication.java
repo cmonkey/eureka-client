1a4b70eafafe64029da5d435e0db08d2ad392359
CMonkey <42.codemonkey@gmail.com>
2022-08-12 18:27:18 +0800

    make EnableReactiveFeignClients Annotation

--- src/main/java/org/excavator/boot/eurekaclient/EurekaClientApplication.java
+++ src/main/java/org/excavator/boot/eurekaclient/EurekaClientApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
+import reactivefeign.spring.config.EnableReactiveFeignClients;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableReactiveFeignClients
 public class EurekaClientApplication {
 
 	public static void main(String[] args) {
