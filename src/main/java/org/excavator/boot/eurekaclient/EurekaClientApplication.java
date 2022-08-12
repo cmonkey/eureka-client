d7434e8cfb0e89631b8416083871214d8449ca59
CMonkey <42.codemonkey@gmail.com>
2022-08-12 16:13:48 +0800

    add EnableEurekaClients Annotation

--- src/main/java/org/excavator/boot/eurekaclient/EurekaClientApplication.java
+++ src/main/java/org/excavator/boot/eurekaclient/EurekaClientApplication.java
@@ -3,9 +3,11 @@ package org.excavator.boot.eurekaclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class EurekaClientApplication {
 
 	public static void main(String[] args) {
